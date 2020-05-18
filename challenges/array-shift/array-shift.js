b2ff3b14a983d8c259c6f4ac8831383723a2de7b
rowaidsayyed <rowaidsayyed@gmail.com>
2020-05-18 21:55:10 +0300

    add stecth goal

--- challenges/array-shift/array-shift.js
+++ challenges/array-shift/array-shift.js
@@ -12,10 +12,27 @@ function insertShiftArray(arr, valueToBeAdded) {
 
   newArr[newArr.length] = valueToBeAdded;
 
-  for (let j = index+1; j< arr.length+1; j++) {
-    newArr[j] = arr[j-1];
+  for (let j = index + 1; j < arr.length + 1; j++) {
+    newArr[j] = arr[j - 1];
   }
   return newArr;
 }
 
-module.exports = insertShiftArray;
+function deleteMiddleElement(arr) {
+
+  let index = 0;
+  if (arr.length % 2 === 0) index = arr.length / 2;
+  else { index = Math.floor(arr.length / 2); }
+
+  for (let i = index; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr.length = arr.length - 1;
+  return arr;
+}
+
+module.exports = {
+  insertShiftArray: insertShiftArray,
+  deleteMiddleElement:deleteMiddleElement,
+} ;
