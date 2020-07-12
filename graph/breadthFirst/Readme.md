4ca71d20a6d38a1d526ccb3fa68aec09edd4b27d
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-13 00:58:03 +0300

    finish breadthFirst

---
+++ graph/breadthFirst/Readme.md
@@ -0,0 +1,23 @@
+## Github actions
+[link](https://github.com/ruwaid-401-advanced-javascript/data-structures-and-algorithms/pull/27/checks)
+
+# graph
+
+Implement Graph Data structure.  The graph should be represented as an adjacency list
+
+## Challenge
+
+* breadthFirst()
+ search in the graph
+## Approach & Efficiency
+
+i used class 
+### BIG O
+#### graph 
+* breadthFirst Method
+  * space --> Big O = O(n) -->  space varies as number of nodes increase or decrease
+  * time --> Big O = O(n) -->  time varies as number of nodes increase or decrease
+
+## Solution
+
+![UML](../assets/graph.png)
---
+++ graph/breadthFirst/breadthFirst.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Graph = require('../graph');
+
+class BreadthFirstGraph extends Graph{
+
+  breadthFirst(startNode) {
+    const queue = [];
+    const vistedNodes = new Set();
+    queue.push(startNode);
+    vistedNodes.add(startNode);
+
+    while (queue.length) {
+      const currentNode = queue.shift();
+
+      const neighbors = this.getNeighbors(currentNode);
+
+      for (let neighbor of neighbors) {
+        const neighborNode = neighbor.vertex;
+        if (vistedNodes.has(neighborNode)) {
+          continue;
+        } else {
+          vistedNodes.add(neighborNode);
+        }
+        queue.push(neighborNode);
+      }
+
+    }
+
+    return vistedNodes;
+  }
+}
+
+module.exports = BreadthFirstGraph;
